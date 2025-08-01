1f7b74b6cf911a3ed1fbccef0bceb5ae75bfabed
Lungile Maseko <90724319+Lu-TheCoder@users.noreply.github.com>
2025-08-01 21:18:03 +0200

    db-integrated to backend

--- database/init.sql
+++ database/init.sql
@@ -9,3 +9,29 @@ CREATE TABLE IF NOT EXISTS users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     role VARCHAR(50) NOT NULL DEFAULT 'user'
 );
+
+CREATE TABLE IF NOT EXISTS vehicle (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    model VARCHAR(50) NOT NULL,
+    year INT NOT NULL,
+    type VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS incident (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    location VARCHAR(120) NOT NULL,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    weather_condition VARCHAR(120) NOT NULL,
+    severity VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS vehicle_risk_properties (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    risk_level VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS route (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    coordinates JSONB NOT NULL
+);
+
