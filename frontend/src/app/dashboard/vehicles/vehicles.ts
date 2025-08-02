cdc03d71a9aa6a65f563b86c0a8cd7b37d1127d6
Hope Tsunke <ttsunke15@gmail.com>
2025-08-02 13:51:08 +0200

    chore: add chart for risk score

--- frontend/src/app/dashboard/vehicles/vehicles.ts
+++ frontend/src/app/dashboard/vehicles/vehicles.ts
@@ -7,15 +7,18 @@ import {
   ReactiveFormsModule,
 } from '@angular/forms';
 import { Vehicle } from '../../services/vehicle/vehicle';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-vehicles',
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FontAwesomeModule],
   templateUrl: './vehicles.html',
   styleUrl: './vehicles.css'
 })
 
 export class Vehicles {
+  faCar = faCar
   vehicleForm: FormGroup
   vehicleService = inject(Vehicle)
 
@@ -45,7 +48,12 @@ export class Vehicles {
   onSubmit(event: Event) {
     event.preventDefault();
     if (this.vehicleForm.valid) {
-      this.vehicleService.createVehicle(this.vehicleForm.value).subscribe(() => { });
+      this.vehicleService.createVehicle(this.vehicleForm.value).subscribe(() => {
+        this.vehicleService.getUserVehicles().subscribe((data) => {
+          this.vehicles = data.data
+        })
+      });
+      this.vehicleForm.reset()
     } else {
       this.vehicleForm.markAllAsTouched();
     }
