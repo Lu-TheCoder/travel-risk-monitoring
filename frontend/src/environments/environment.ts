6d1e9962920b0ebf19a9d7b87b2cc032204dc2c7
Lungile Maseko <90724319+Lu-TheCoder@users.noreply.github.com>
2025-08-02 08:33:19 +0200

    fixes a cors issue with fetching from client so we now fetch data from backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   googleMapsApiKey: 'AIzaSyCKr3-DnJeKiPQpxP-TVk1WJR6-DXZzTdk',
-  openWeatherMapApiKey: 'e7deb1aa6862dd69ccbac48f300fe4f0', // Replace with your actual API key
-  googleMapsMapId: 'DEMO_MAP_ID' // Map ID for Advanced Markers
+  googleMapsMapId: 'DEMO_MAP_ID', // Map ID for Advanced Markers
+  backendUrl: 'http://localhost:3000' // Backend API URL
 }; 
